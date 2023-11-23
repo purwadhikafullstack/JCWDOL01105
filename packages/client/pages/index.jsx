3f6a6211dfd3725db00eb538da5662749c1d84ce
Leon <leon.rambing10@gmail.com>
2023-11-23 17:02:37 +0800

    Squashed commit of the following:
    commit a2881dc90a0e47a70fde0282081a1fd2dfb795f3
    Author: Leon <leon.rambing10@gmail.com>
    Date:   Tue Nov 21 20:00:30 2023 +0800
    
        Landing page
    
    commit 06af02558b529835fad863e744ca85260337e7ec
    Author: Leon <leon.rambing10@gmail.com>
    Date:   Tue Nov 21 19:13:13 2023 +0800
    
        Landing Page

--- packages/client/pages/index.jsx
+++ packages/client/pages/index.jsx
@@ -1,8 +1,8 @@
 import Footer from '../src/components/Footer/Footer';
-// import Modal from '../src/components/Modals/Modal';
+import Modal from '../src/components/Modals/Modal';
 import Navbar from '../src/components/Navbar/Navbar';
 import Slider from '../src/components/Slider/Slider';
-// import Destination from './app/Location/Destination';
+import Destination from './app/Location/Destination';
 import Page from './app/page';
 
 const Home = () => {
