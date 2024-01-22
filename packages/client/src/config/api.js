d8a926c7b710fa836cade64f8ea64f3a9a820e8c
Ilham Hidayatulloh <114216025+Ilhamhidayatulloh59@users.noreply.github.com>
2024-01-22 15:13:01 +0700

    fix deploy

--- packages/client/src/config/api.js
+++ packages/client/src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API_URL =
-  process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
+  process.env.NEXT_APP_API_BASE_URL || 'http://localhost:8080/api';
 
 const api = axios.create({
   baseURL: API_URL,
